bf6ce751e15f27e9b179e434b5eb1b6e71cd6d18
u0525087 <u0525087@667efa29-2a12-4242-877e-11a4430a8232>
2012-03-09 20:49:50 +0000

    Add MainFrame
    git-svn-id: https://sat.inso.tuwien.ac.at/repositories/svn/sepm_ss12_0525087@6 667efa29-2a12-4242-877e-11a4430a8232

---
+++ sepm/src/gui/MainFrame.java
@@ -0,0 +1,42 @@
+package gui;
+
+import org.apache.log4j.Logger;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.UIManager;
+
+public class MainFrame extends JFrame{
+
+	private static final long serialVersionUID = 4714864527745266449L;
+	private Logger logger = Logger.getLogger("gui.MainFrame.class");
+
+	private JDesktopPane mypane = new JDesktopPane();
+	private JList list = new JList();
+	
+	public MainFrame(String title){
+		super(title);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				try{
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				}
+				catch (Exception e){
+					//TODO
+					e.printStackTrace();
+				}
+				MainFrame frame = new MainFrame("Edis Gui");
+				frame.setVisible(true);
+			}
+		});
+
+	}
+
+}
