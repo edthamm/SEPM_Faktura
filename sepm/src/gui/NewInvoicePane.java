7a050423e848718691f6cc95e2e887f40b591845
u0525087 <u0525087@667efa29-2a12-4242-877e-11a4430a8232>
2012-03-10 19:02:35 +0000

    add class
    git-svn-id: https://sat.inso.tuwien.ac.at/repositories/svn/sepm_ss12_0525087@10 667efa29-2a12-4242-877e-11a4430a8232

---
+++ sepm/src/gui/NewInvoicePane.java
@@ -0,0 +1,18 @@
+package gui;
+
+import org.apache.log4j.Logger;
+
+import javax.swing.JPanel;
+
+public class NewInvoicePane extends JPanel{
+
+	private static final long serialVersionUID = 4592563781982348661L;
+	private Logger logger = Logger.getLogger("gui.NewInvoicePane.class");
+	
+	public NewInvoicePane(){
+		super();
+		
+	}
+	
+
+}
