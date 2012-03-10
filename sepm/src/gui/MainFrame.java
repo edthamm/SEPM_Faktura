1cfb27a5e99faf0c25447a796d2bccba0bd04b59
u0525087 <u0525087@667efa29-2a12-4242-877e-11a4430a8232>
2012-03-10 17:52:08 +0000

    create my tabs
    git-svn-id: https://sat.inso.tuwien.ac.at/repositories/svn/sepm_ss12_0525087@9 667efa29-2a12-4242-877e-11a4430a8232

--- sepm/src/gui/MainFrame.java
+++ sepm/src/gui/MainFrame.java
@@ -1,12 +1,13 @@
 package gui;
 
+
 import net.miginfocom.swing.MigLayout;
 
 import org.apache.log4j.Logger;
 
-import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
-import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 
 public class MainFrame extends JFrame{
@@ -14,18 +15,28 @@ public class MainFrame extends JFrame{
 	private static final long serialVersionUID = 4714864527745266449L;
 	private Logger logger = Logger.getLogger("gui.MainFrame.class");
 
-	private JDesktopPane mypane = new JDesktopPane();
-	private JList list = new JList();
+	private JTabbedPane tabs = new JTabbedPane(JTabbedPane.TOP);
 	
 	public MainFrame(String title){
 		super(title);
+		
+		logger.info("Setting Layout");
+		MigLayout layout = new MigLayout();
+		setLayout(layout);
+		
+		logger.info("Adding tabs");
+		tabs.addTab("Rechnung erstellen", new JPanel());
+		tabs.addTab("Rechnungen verwalten", new JPanel());
+		tabs.addTab("Artikel verwalten", new JPanel());
+		tabs.addTab("Benutzer verwalten", new JPanel());
+		
+		logger.info("Making tabs visible");
+		add(tabs,"grow, push");
+		
+		logger.info("Setting default behavior");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		
-		//TODO make a layout
-		MigLayout layout = new MigLayout();
-		setLayout(layout);
 		
 	}
 	
