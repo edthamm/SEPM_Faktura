db46cffbe240f872ce3d4fa84659800300cd293c
u0525087 <u0525087@667efa29-2a12-4242-877e-11a4430a8232>
2012-03-14 11:03:04 +0000

    implement create invoice
    git-svn-id: https://sat.inso.tuwien.ac.at/repositories/svn/sepm_ss12_0525087@44 667efa29-2a12-4242-877e-11a4430a8232

--- sepm/src/dao/InvoiceDAO.java
+++ sepm/src/dao/InvoiceDAO.java
@@ -1,15 +1,14 @@
 package dao;
 
-import java.sql.Date;
 import java.util.List;
 
 import entities.Invoice;
 
 public interface InvoiceDAO {
-	public Invoice createInvoice(String date, String time, String waiter);
+	public Invoice createInvoice(String date, String time, String waiter) throws JDBCInvoiceDAOImplException;
 	public Invoice updateInvoice(Invoice toUpdate);
 	public void deleteInvoice(Invoice toDelete);
 	public List<Invoice> findAll();
 	public Invoice findById(int id);
-	public List<Invoice> findByDate(Date date);
+	public List<Invoice> findByDate(String date);
 }
--- sepm/src/dao/JDBCInvoiceDAOImpl.java
+++ sepm/src/dao/JDBCInvoiceDAOImpl.java
@@ -1,7 +1,8 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,10 +14,19 @@ import entities.InvoiceImpl;
 
 public class JDBCInvoiceDAOImpl implements InvoiceDAO {
 	
+	@SuppressWarnings("unused")
 	private DatabaseConnector dbc;
 	private Connection c = null;
 	private Logger logger = Logger.getLogger("dao.JDBCInvoiceDAOImpl.class");
 	
+	private PreparedStatement createStatement;
+	private PreparedStatement getIdAfterCreate;
+	
+	/**
+	 * 
+	 * @param dbc
+	 * @throws JDBCInvoiceDAOImplException
+	 */
 	public JDBCInvoiceDAOImpl(DatabaseConnector dbc) throws JDBCInvoiceDAOImplException{
 		logger.info("Initializing new JDBCInvoiceDAOImpl");
 		this.dbc = dbc;
@@ -27,14 +37,37 @@ public class JDBCInvoiceDAOImpl implements InvoiceDAO {
 			logger.error("Error getting a Connection from Database");
 			throw new JDBCInvoiceDAOImplException("Could not get Database connection");
 		}
+		
+		try {
+			prepareStatements();
+		} catch (SQLException e) {
+			logger.error("Could not prepare statements");
+			throw new JDBCInvoiceDAOImplException("Error preparing Statements");
+		}
+	}
+	
+	private void prepareStatements() throws SQLException {
+		createStatement = c.prepareStatement("INSERT INTO INVOICE VALUES (NULL,?,0,?,?)");//date waiter time
+		getIdAfterCreate = c.prepareStatement("SELECT iid FROM invoice WHERE total = 0 AND" +
+											  " date = ? AND waiter = ? AND time = ?");
+		
 	}
 
 	@Override
-	public Invoice createInvoice(String date, String time, String waiter) {
-		// TODO Auto-generated method stub
+	public Invoice createInvoice(String date, String time, String waiter) throws JDBCInvoiceDAOImplException {
+		
 		InvoiceImpl newInvoice = createInvoiceObject(date, time, waiter);
-		int invoiceid = writeInvoiceToDatabase(newInvoice);
-		newInvoice.setId(invoiceid);
+		
+		try {
+			
+			int invoiceid = writeInvoiceToDatabase(newInvoice);
+			newInvoice.setId(invoiceid);
+		
+		} catch (SQLException e) {
+			logger.error("Could not insert new Invoice in to Database");
+			throw new JDBCInvoiceDAOImplException("Error inserting in to Database");
+		}
+		
 		return newInvoice;
 	}
 
@@ -42,9 +75,22 @@ public class JDBCInvoiceDAOImpl implements InvoiceDAO {
 		return new InvoiceImpl(date,time,waiter,0);
 	}
 
-	private int writeInvoiceToDatabase(Invoice newInvoice) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int writeInvoiceToDatabase(Invoice newInvoice) throws SQLException {
+		createStatement.setDate(1, newInvoice.getDate());
+		createStatement.setString(2, newInvoice.getWaiter());
+		createStatement.setTime(3, newInvoice.getTime());
+		
+		createStatement.executeUpdate();
+		
+		getIdAfterCreate.setDate(1, newInvoice.getDate());
+		getIdAfterCreate.setString(2, newInvoice.getWaiter());
+		getIdAfterCreate.setTime(3, newInvoice.getTime());
+		
+		ResultSet r = getIdAfterCreate.executeQuery();
+		r.next();
+		int id = r.getInt("iid");
+		logger.debug("Returning id: "+id);
+		return id;
 	}
 
 	@Override
@@ -72,7 +118,7 @@ public class JDBCInvoiceDAOImpl implements InvoiceDAO {
 	}
 
 	@Override
-	public List<Invoice> findByDate(Date date) {
+	public List<Invoice> findByDate(String date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
